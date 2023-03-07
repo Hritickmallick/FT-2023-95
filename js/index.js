ccfc3aa9ea263be3029568f732e8fc34f67ea050
hritick <hritick.mallick@innoraft.com>
2023-03-07 12:17:38 +0530

    changes done according to review

--- js/index.js
+++ js/index.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
         infinite: true,
         arrows:true,
         centerMode: true,
-        centerPadding: '20px',
+        centerPadding: '0px',
         slidesToShow: 2,
         prevArrow:'<i class="fa-solid fa-arrow-left prev-arrow arrow"></i>',
         nextArrow: '<i class="fa-solid fa-arrow-right next-arrow arrow"></i>',
@@ -20,15 +20,17 @@ $(document).ready(function(){
           {
             breakpoint: 768,
             settings: {
-              slidesToShow: 2 ,    
+              slidesToShow: 1 , 
+              infinite: true,
+              centerMode: true,
+              arrows:true   
             }
           },
           {
             breakpoint: 568,
             settings: {
               slidesToShow: 1,
-              slidesToScroll:1,
-             centerMode:false
+             centerMode:true
             }
           }
         ]
@@ -40,6 +42,8 @@ $(document).ready(function(){
         infinite: true,
         centerMode: true,
         centerPadding: '0px',
+        autoplay:true,
+        autoplaySpeed:1000,
         slidesToShow: 3,
         responsive: [
           {
